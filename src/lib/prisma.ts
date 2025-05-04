98cd33ae162feb01ba607db7d83656ef26a0e9b9
brenolapreza <wefit.breno@voeazul.com.br>
2025-05-04 18:45:47 -0300

    fix: ajusta db para postgres com supabase

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,35 @@
 import { PrismaBetterSQLite3 } from "@prisma/adapter-better-sqlite3";
 import { PrismaClient } from "@prisma/client";
+import { sections } from "@/app/(public)/section";
 
 const adapter = new PrismaBetterSQLite3({
   url: "file:./prisma/dev.db",
 });
 
 export const prisma = new PrismaClient({ adapter });
+
+async function main() {
+  for (const section of sections) {
+    await prisma.section.create({
+      data: {
+        section: section.section,
+        title: section.title ?? null,
+        subtitle: section.subtitle ?? null,
+        content: section.content ?? null,
+        button: section.button ?? null,
+        cards: section.cards ?? null,
+        buttons: section.buttons ?? null,
+      },
+    });
+  }
+}
+
+main()
+  .then(() => {
+    console.log("Seed finalizado com sucesso!");
+    return prisma.$disconnect();
+  })
+  .catch((err) => {
+    console.error(err);
+    return prisma.$disconnect();
+  });
