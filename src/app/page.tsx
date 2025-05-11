e8b5a3c38f9b0c44eeb0e182ea7c16633eb4852c
brenolapreza <wefit.breno@voeazul.com.br>
2025-05-11 17:39:24 -0300

    feat: page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import type { Key } from "react";
 
 export default async function Home() {
   const mdBlocks = await n2m.pageToMarkdown(
-    "1f0d872b-594c-80ab-acb1-0037ce171424"
+    "1f034ae0-7f65-81f6-bcaa-f1d1628f012a"
   );
   const mdString = n2m.toMarkdownString(mdBlocks);
 
